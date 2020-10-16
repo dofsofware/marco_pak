3047423722daa93f4bb5c68391513916464a352c
unknown <xamalteam@gmail.com>
2020-10-16 08:45:03 +0000

    test js

---
+++ src/main/webapp/content/hello.js
@@ -0,0 +1,11 @@
+
+// window.onload=function() {
+    
+//     // btntest = document.getElementById("btntest");
+//     // btntest.onclick = function () {
+//     //     alert('Hello, world!');
+//     // }
+// }
+// $(document).ready(function(){
+//     alert('Hello, world!');
+// });
